ecb975d9f68749f5f20b958ae445164a4eb84cb4
Xing Xing <mikespook@gmail.com>
2014-01-10 11:01:21 +0800

    fixed usage of build-all

--- build-all.sh
+++ build-all.sh
@@ -3,7 +3,7 @@ current="`pwd`"
 dist="saucy raring quantal precise lucid"
 
 usage() {
-    echo "Usage: `basename $0` [-v $VERSION] [-i]"
+    echo "Usage: `basename $0` [-v] [-i]"
 	printf "\t -v : Upstream version\n"
  	printf "\t -i : Index version\n"
     exit 1
@@ -19,6 +19,11 @@ while getopts 'v:i:' o &>> /dev/null; do
         usage;;
     esac
 done
+
+if [ -z $INC ] || [ -z $VERSION ]; then
+	usage
+	exit 1
+fi
 
 for d in $dist; do
 	$current/build.sh -v $VERSION -i $INC -c $dist
